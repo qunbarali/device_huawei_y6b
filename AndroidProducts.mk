e0522e12ea14410fa5fabb23e9fe6b3f029e2a18
S4r4-77 <saraa.j8888@gmail.com>
2026-02-13 21:36:05 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,8 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# List of makefiles that define products in this directory
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_y6b.mk
 
+# Lunch choices that will appear in the lunch menu
 COMMON_LUNCH_CHOICES := \
-    twrp_y6b-eng
+    twrp_y6b-eng \
+    twrp_y6b-userdebug
+
+# Optional: you can add more lunch combos later
+# COMMON_LUNCH_CHOICES += \
+#     twrp_y6b-user
